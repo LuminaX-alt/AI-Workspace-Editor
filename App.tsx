f9e1ebd3b26e2bc532f8e52103334822b1b11b72
Dev Sharma <12devsharma10c@gmail.com>
2025-07-19 01:16:34 +0530

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,17 @@
+
+import { LiveObject } from "@liveblocks/client";
+import { RoomProvider } from "./liveblocks.config";
+import { Room } from "./Room";
+import { ClientSideSuspense } from "@liveblocks/react";
+
+export default function App() {
+  const roomId = "{% ROOM_ID %}";
+
+  return (
+    <RoomProvider id={roomId}>
+      <ClientSideSuspense fallback={<div>Loading…</div>}>
+        <Room />
+      </ClientSideSuspense>
+    </RoomProvider>
+  );
+}
